0ef67b6267532a1df9374b145284e2b170290bd4
Azhar <mohdazharsm@gmail.com>
2023-06-17 07:18:01 +0530

    Api key updated

--- ai_triad_flutter/lib/constants/app_keys.dart
+++ ai_triad_flutter/lib/constants/app_keys.dart
@@ -3,4 +3,4 @@ const String UsersFirestoreKey = 'users';
 const String TokenFirestoreKey = 'token';
 // const String ChatsFirestoreKey = 'chats';
 // const String FilesFirestoreKey = 'files';
-const gptToken = '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';
+const gptToken = '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';
