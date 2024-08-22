d61a3e43c0edf2f861b2ee7d0381b842e7857b82
fayiz120 <mfaisep@gmail.com>
2024-08-22 18:31:32 +0530

    no message

---
+++ ai_triad_flutter/lib/constants/app_keys.dart
@@ -0,0 +1,6 @@
+/// Firestore Keys
+const String UsersFirestoreKey = 'users';
+const String TokenFirestoreKey = 'token';
+const String AdminFirestoreKey = 'admin';
+// const String ChatsFirestoreKey = 'chats';
+// const String FilesFirestoreKey = 'files';
