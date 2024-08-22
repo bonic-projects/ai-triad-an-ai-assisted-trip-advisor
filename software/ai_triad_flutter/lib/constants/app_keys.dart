ac4f3ddfd218f9ad18fd6a9f5f16da5840f976a8
fayiz120 <mfaisep@gmail.com>
2024-08-22 18:29:57 +0530

    keys removed

--- software/ai_triad_flutter/lib/constants/app_keys.dart
+++
@@ -1,6 +0,0 @@
-/// Firestore Keys
-const String UsersFirestoreKey = 'users';
-const String TokenFirestoreKey = 'token';
-const String AdminFirestoreKey = 'admin';
-// const String ChatsFirestoreKey = 'chats';
-// const String FilesFirestoreKey = 'files';
